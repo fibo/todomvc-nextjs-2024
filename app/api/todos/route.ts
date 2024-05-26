0f9068691ea09aef4ec0c3a3830b40416fddc5e2
fibo <fibo@users.noreply.github.com>
2024-05-26 23:23:01 +0200

    create db.json if it does not exist

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,9 +1,18 @@
 import read from "read-file-utf8";
+import write from "write-file-utf8";
 import { Todo } from "@/app/models";
 
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-  const data = await read<{ todos: Todo[] }>("db.json");
-  return Response.json(data.todos);
+  try {
+    const data = await read<{ todos: Todo[] }>("db.json");
+    return Response.json(data.todos);
+  } catch (error) {
+    if ((error as NodeJS.ErrnoException).code === "ENOENT") {
+      const todos: Todo[] = [];
+      await write("db.json", JSON.stringify({ todos }));
+      return Response.json(todos);
+    }
+  }
 }
